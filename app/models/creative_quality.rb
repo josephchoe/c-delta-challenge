6ae37e0398f399eb67f2ebf490c18b89c5210a6a
Joseph Choe <joseph@josephchoe.com>
2018-07-20 18:32:50 -0700

    1.3 Scoring Creative Qualities globally

--- app/models/creative_quality.rb
+++ app/models/creative_quality.rb
@@ -2,4 +2,16 @@ class CreativeQuality < ApplicationRecord
   has_many :question_choices
 
   validates :name, :description, :color, presence: true
+
+  def score
+    question_responses_for_creative_quality = QuestionResponse.joins(:question_choice).merge(self.question_choices)
+    score = question_responses_for_creative_quality.inject(Hash.new(0.0)) do |accumulator, question_response|
+      accumulator.tap do |acc|
+        acc[:raw_score] += question_response.question_choice.score
+        acc[:max_score] += question_response.question.question_choices.maximum(:score)
+      end
+    end
+
+    ((score[:raw_score] / score[:max_score]) * 100).to_i
+  end
 end
--- spec/models/creative_quality_spec.rb
+++ spec/models/creative_quality_spec.rb
@@ -9,4 +9,30 @@ describe CreativeQuality do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:description) }
   end
+
+  describe '#score' do
+    let(:creative_quality) { create(:creative_quality) }
+    let(:question1) { create(:question) }
+    let!(:question1_choice1) { create(:question_choice, question: question1, creative_quality: creative_quality, score: 1) }
+    let!(:question1_choice2) { create(:question_choice, question: question1, creative_quality: creative_quality, score: -1) }
+    let!(:question1_choice3) { create(:question_choice, question: question1, creative_quality: creative_quality, score: 4) }
+    let(:question2) { create(:question) }
+    let!(:question2_choice1) { create(:question_choice, question: question2, creative_quality: creative_quality, score: 1) }
+    let!(:question2_choice2) { create(:question_choice, question: question2, creative_quality: creative_quality, score: -1) }
+    let!(:question2_choice3) { create(:question_choice, question: question2, creative_quality: creative_quality, score: 4) }
+    let(:question3) { create(:question) }
+    let!(:question3_choice1) { create(:question_choice, question: question3, creative_quality: creative_quality, score: 1) }
+    let!(:question3_choice2) { create(:question_choice, question: question3, creative_quality: creative_quality, score: -1) }
+    let!(:question3_choice3) { create(:question_choice, question: question3, creative_quality: creative_quality, score: 4) }
+    let(:response1) { create(:response) }
+    let(:response2) { create(:response) }
+    let(:response3) { create(:response) }
+
+    it 'adds choice scores to total' do
+      create(:question_response, response: response1, question_choice: question1_choice2)
+      create(:question_response, response: response2, question_choice: question2_choice1)
+      create(:question_response, response: response3, question_choice: question3_choice3)
+      expect(creative_quality.score).to eql 33
+    end
+  end
 end
